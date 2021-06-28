3b99f47d20e918a889fb1c6f5e30b0253d17889e
[twasileva] <[twasileva@gmail.com]>
2021-06-28 18:30:10 +0300

    +++

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 import "./styles/main.scss";
+import template from "./templates/main.hbs"
+import data from "./data/data.json"
+import { setLocalStorage } from "./utils/utils";
+import { getLocalStorage } from "./utils/utils";
+
+let tmpNameUser = ''
+
+window.onload = () => {
+  const container = document.getElementById('container');
+  const isName = getLocalStorage()
+  if (isName) {
+    const newDate = Object.assign({}, data, { name: isName })
+    container.innerHTML = template(newDate)
+  } else {
+    container.innerHTML = template(data);
+    const nameInput = document.getElementById('nameInput')
+    const saveUser = document.getElementById('saveUser')
+
+    nameInput.addEventListener('input', e => {
+      console.log(e.target.value);
+      tmpNameUser = e.target.value
+    })
+    saveUser.addEventListener('click', () => {
+      setLocalStorage(tmpNameUser)
+    })
+  }
+}
+
+const output = document.querySelector('#js-output');
+const input = document.querySelector('#js-input');
+const btn = document.querySelector('#js-btn');
+
+const updateView = () => {
+  output.textContent = localStorage.getItem('goit-js-module-12') || '';
+};
+
+const handleClick = () => {
+  localStorage.setItem('goit-js-module-12', input.value);
+  updateView();
+};
+
+updateView();
+
+btn.addEventListener('click', handleClick);
+// localStorage.clear()
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,19 @@
-export const test = () => {
-  console.log("lll");
+import { USER_NAME } from "./constants";
+export const setLocalStorage = (name) => {
+  try {
+    localStorage.setItem(USER_NAME, name)
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+export const getLocalStorage = () => {
+  try {
+    const isNameExists = localStorage.getItem(USER_NAME)
+    if (!isNameExists) return false
+    return isNameExists
+
+  } catch (error) {
+    console.log(error);
+  }
+}
